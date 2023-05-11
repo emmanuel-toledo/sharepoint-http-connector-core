fd0811985f2420100b0dba927d045626a03aa645
Emmanuel Toledo <emmanueltoledo1082@gmail.com>
2023-05-10 18:18:21 -0600

    Created Unit Test project with different files.

---
+++ SharePoint.Connector.Core.UT/SiteUsage.cs
@@ -0,0 +1,36 @@
+﻿namespace SharePoint.Connector.Core.UT
+{
+    [TestClass]
+    public class SiteUsage
+    {
+        private IServiceProvider _provider;
+
+        private IServiceCollection _services;
+
+        private ISharePointContext _context;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            _services = new ServiceCollection();
+            _services.UseSharePointSite(new ContextConfiguration()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Site",
+                TenantId = "346a1d1d-e75b-4753-902b-74ed60ae77a1",
+                ClientId = "4a929590-75e1-4fda-b9d2-9f5acbf8251e@346a1d1d-e75b-4753-902b-74ed60ae77a1",
+                ClientSecret = "CqUv/BdSw0nLfqccLoPiFvQJDAbfXMvIoPpn+tw54rY=",
+                SharePointSiteURL = "https://laureatelatammx.sharepoint.com/sites/uvm-expediente-digital-dev/"
+            });
+            _provider = _services.BuildServiceProvider();
+            _context = _provider.GetService<ISharePointContext>()!;
+        }
+
+        [TestMethod]
+        public async Task Get_Site_Usage()
+        {
+            var response = await _context.SiteUsageAsync();
+            Assert.IsNotNull(response);
+        }
+    }
+}
